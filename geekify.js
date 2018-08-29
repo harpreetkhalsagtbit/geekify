2f18a3ece21150e249575d423c9582ffb9e22f1c
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2018-08-29 15:30:35 +0530

    converted all func to new promise code

--- geekify.js
+++ geekify.js
@@ -10,67 +10,35 @@ program
     .command('init')
     .description('Reset all changes and Initialize')
     .action(function(name) {
-        geekHandler.printLibName();
-        geekHandler.geekPerformInitAction().then(function() {
-            console.log("Init done")
-        }, function(err) {
-            console.error("geekPerformInitAction: ", err)
-        }).catch(function(err) {
-            console.log(err.stack);
-        });
+        geekHandler.geekPerformInitAction()
     });
 
 program
     .command('reset')
     .description('Reset all changes')
     .action(function(name) {
-        geekHandler.printLibName();
-        geekHandler.geekPerformResetAction().then(function() {
-            console.log("Reset done")
-        }, function(err) {
-            console.error("geekPerformResetAction: ", err)
-        }).catch(function(err) {
-            console.log(err.stack);
-        });
+        geekHandler.geekPerformResetAction()
     });
 
 program
     .command('showdiff')
     .description('show diff between current and last commit - defined in geek.json file of current repository')
     .action(function(name) {
-        geekHandler.geekPerformShowDiffAction().then(function() {
-            console.log("Diff done")
-        }, function(err) {
-            console.error("geekPerformShowDiffAction: ", err)
-        }).catch(function(err) {
-            console.log(err.stack);
-        });
+        geekHandler.geekPerformShowDiffAction()
     });
 
 program
     .command('next')
     .description('Jumt to Next Step - defined in geek.json file of current repository')
     .action(function(name) {
-        geekHandler.geekPerformNextAction().then(function() {
-            console.log("Next done")
-        }, function(err) {
-            console.error("geekPerformNextAction: ", err)
-        }).catch(function(err) {
-            console.log(err.stack);
-        });
+        geekHandler.geekPerformNextAction()
     });
 
 program
     .command('previous')
     .description('Jumt to Previous Step - defined in geek.json file of current repository')
     .action(function(name) {
-        geekHandler.geekPerformPreviousAction().then(function() {
-            console.log("Previous done")
-        }, function(err) {
-            console.error("geekPerformPreviousAction: ", err)
-        }).catch(function(err) {
-            console.log(err.stack);
-        });
+        geekHandler.geekPerformPreviousAction()
     });
 
 
