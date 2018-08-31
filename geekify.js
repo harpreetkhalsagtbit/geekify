c47baede77def41a7f6b08122c1b42114edd5e67
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2018-08-31 15:06:44 +0530

    Add: jump tp cmd

--- geekify.js
+++ geekify.js
@@ -9,37 +9,43 @@ program
 program
     .command('init')
     .description('Reset all changes and Initialize')
-    .action(function(name) {
+    .action(function (name) {
         geekHandler.geekPerformInitAction()
     });
 
 program
     .command('reset')
     .description('Reset all changes')
-    .action(function(name) {
+    .action(function (name) {
         geekHandler.geekPerformResetAction()
     });
 
 program
     .command('showdiff')
     .description('show diff between current and last commit - defined in geek.json file of current repository')
-    .action(function(name) {
+    .action(function (name) {
         geekHandler.geekPerformShowDiffAction()
     });
 
 program
     .command('next')
-    .description('Jumt to Next Step - defined in geek.json file of current repository')
-    .action(function(name) {
+    .description('Jump to Next Step - defined in geek.json file of current repository')
+    .action(function (name) {
         geekHandler.geekPerformNextAction()
     });
 
 program
     .command('previous')
-    .description('Jumt to Previous Step - defined in geek.json file of current repository')
-    .action(function(name) {
+    .description('Jump to Previous Step - defined in geek.json file of current repository')
+    .action(function (name) {
         geekHandler.geekPerformPreviousAction()
     });
 
+program
+    .command('jumpto <branchName>')
+    .description('Jump to specific Step - defined in geek.json file of current repository')
+    .action(function (branchName) {
+        geekHandler.geekPerformJumpToAction(branchName)
+    });
 
 program.parse(process.argv);
